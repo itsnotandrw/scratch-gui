2a6e48f66e0a8f14f6abc218810b353954070717
Andres F Moncada <andresxs.2219@gmail.com>
2025-11-09 19:31:29 -0500

    branding: reemplazo logo y variables CSS de branding

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// arriba del todo, junto a otros imports
+import './styles/branding.css';
 import GUI from './containers/gui.jsx';
 import AppStateHOC from './lib/app-state-hoc.jsx';
 import GuiReducer, {guiInitialState, guiMiddleware, initEmbedded, initFullScreen, initPlayer} from './reducers/gui';
@@ -7,6 +9,7 @@ import {setFullScreen, setPlayer} from './reducers/mode';
 import {remixProject} from './reducers/project-state';
 import {setAppElement} from 'react-modal';
 import {activateDeck} from './reducers/cards.js';
+
 
 const guiReducers = {
     locales: LocalesReducer,
